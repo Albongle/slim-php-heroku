b09fd34b72791c65c83e19a98187522240520569
Albongle <alejandro.bongioanni@gmail.com>
2021-04-06 15:12:05 -0300

    Ejercicio

--- app/index.php
+++ app/index.php
@@ -7,6 +7,6 @@
     <title>Alejandro Bongioanni</title>
 </head>
 <body>
-<iframe src="https://www.youtube.com/embed/N7otDLzQCug?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="1280" height="1024" src="https://www.youtube.com/embed/N7otDLzQCug?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
 </html>
